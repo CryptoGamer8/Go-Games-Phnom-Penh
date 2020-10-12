b5dcca05e9a54fddabdaf62969fcea2ab7b52c8e
hubertzk <hubertzk@163.com>
2020-10-12 15:52:48 +0800

    cannot connect to mongodb

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
 	hostname:'127.0.0.1',
-	port:'3000',
+	port:'3000'
 	// _dirname:'$home/Downloads/goboard/HTML'
 };
 
--- src/myApp.js
+++ src/myApp.js
@@ -1,29 +1,46 @@
 const http = require('http');
 const config = require('./config.js').config
 const fs = require('fs');
+const ejs = require('ejs');
+const qs = require('querystring');
 
 const hostname = config.hostname;
 const port = config.port;	//port大于1024随便定
-// const _dirname = config._dirname;
+var template = fs.readFileSync('./HTML/forum.ejs','utf-8');
+var posts = [];
 
-//request, response
 const server = http.createServer((req,res) => {
-	fs.readFile('./HTML/index.html','utf-8',function(err,data){
-		if(err){
-			res.setHeader('Content-Type','text/plain');
-			res.statusCode = 404;
-			res.end('Not Found');
-			console.log('Request url not found');
-		}
-		else{
-			res.setHeader('Content-Type','text/html');
-			res.statusCode = 200;
-			res.end(data);
-			console.log('find index.html successfully!');
-		}
-	})
-})
+	if(req.method === 'POST'){
+		req.data = "";
+		req.on("readable",function(){
+			//表单数据收集
+			var chr = req.read();
+			if(chr)
+				req.data += chr;
+		});
+		req.on("end",function(){
+			//表单处理
+			var query = qs.parse(req.data);
+			posts.push(query.content);
+			showForm(posts,res);
+		})
+	}
+	else{
+		showForm(posts,res);
+	}
+});
 
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function showForm(p_posts,res){
+	var data = ejs.render(template,{
+		title: 'hello ejs',
+		posts: p_posts
+	});
+	res.setHeader('Content-Type','text/html');
+	res.statusCode = 200;
+	res.end(data);
+}
+
