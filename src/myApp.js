8ec7bed68f48d83361786e8f003f3ff6ecf93247
hubertzk <hubertzk@163.com>
2020-10-13 16:00:07 +0800

    chess board picture not found

--- src/myApp.js
+++ src/myApp.js
@@ -6,38 +6,22 @@ const qs = require('querystring');
 
 const hostname = config.hostname;
 const port = config.port;	//port大于1024随便定
-var template = fs.readFileSync('./HTML/forum.ejs','utf-8');
-var posts = [];
 
 const server = http.createServer((req,res) => {
-
-	console.log('req.url:' + req.url)
-	fs.readFile('./HTML/flap.html','utf-8',function(err,data){
+	fs.readFile('./HTML/goBoard.html','utf-8',function(err,data){
 		if(err){
 			res.setHeader('Content-Type','text/plain');
 			res.statusCode = 404;
 			res.end('Not Found');
-			console.log(err)
 		}
 		else{
 			res.setHeader('Content-Type','text/html');
 			res.statusCode = 200;
 			res.end(data);
-			console.log('find flap.html successfully!');
 		}
-	})
+	});
 });
 
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-function showForm(p_posts,res){
-	var data = ejs.render(template,{
-		title: 'hello ejs',
-		posts: p_posts
-	});
-	res.setHeader('Content-Type','text/html');
-	res.statusCode = 200;
-	res.end(data);
-}
