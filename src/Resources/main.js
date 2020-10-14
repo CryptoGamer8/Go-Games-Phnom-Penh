30dfbe3399d6ae221d6e7edf34bb6c7536928171
hubertzk <hubertzk@163.com>
2020-10-14 18:35:19 +0800

    organize code structure

--- src/Resources/main.js
+++ src/Resources/main.js
@@ -1,29 +1,34 @@
 const socket = io();
 
+const gridSide = 76 //side length of board grid in px
+const topLeft = [45, 45] //coordinate of top left corner of gameboard, in X, Y
+const activePlayers = [`black`, `white`]
+
 var goboard = document.getElementById("goboard");
 var player = `Viewer`;
 var userName;
 
-const activePlayers = [`black`, `white`]
 const placePiece = (color, top, left) => {
     const piece = `<div class="${color}ChessPiece"
          style="position:absolute; top:${top}px; left:${left}px" >
     </div>`;
     goboard.innerHTML += piece;
 }
-const gridSide = 76 //side length of board grid in px
-const topLeft = [45, 45] //coordinate of top left corner of gameboard, in X, Y
+
 const boardClick = (data) => {
     socket.emit("place piece", [player, data.offsetY, data.offsetX])
     console.log(data.offsetX, data.offsetY)
 }
+
 const newUserConnected = (user) => {
     userName = user || `User${Math.floor(Math.random() * 1000000)}`;
     socket.emit("new user", userName);
 };
+
 socket.on("new user", function (data) {
 
 });
+
 socket.on("assign side", function(data){
     if (data[1] != userName){
         return
@@ -34,6 +39,7 @@ socket.on("assign side", function(data){
         goboard.addEventListener("click", boardClick);
     }
 })
+
 socket.on("place piece", function(data){
     placePiece(data[0], data[1], data[2])
 })
@@ -44,5 +50,3 @@ socket.on("user disconnected", function (userName) {
 
 //Game starts here
 newUserConnected();
-
-
--- src/myApp.js
+++ src/myApp.js
@@ -1,53 +1,24 @@
-// const http = require('http');
-// const config = require('./config.js').config
-// const fs = require('fs');
-// const ejs = require('ejs');
-// const qs = require('querystring');
-//
-// const hostname = config.hostname;
-// const port = config.port;	//port大于1024随便定
-//
-// const server = http.createServer((req,res) => {
-// 	fs.readFile('./HTML/index.html','utf-8',function(err,data){
-// 		if(err){
-// 			res.setHeader('Content-Type','text/plain');
-// 			res.statusCode = 404;
-// 			res.end('Not Found');
-// 		}
-// 		else{
-// 			res.setHeader('Content-Type','text/html');
-// 			res.statusCode = 200;
-// 			res.end(data);
-// 		}
-// 	});
-// });
-//
-// server.listen(port, hostname, () => {
-// 	console.log(`Server running at http://${hostname}:${port}/`);
-// });
 const express = require("express");
 const socket = require("socket.io");
 const config = require("./config.js").config;
 
 // App setup
 const PORT = config.port;
+const publicPath = config.publicPath;
 const app = express();
 const server = app.listen(PORT, function () {
-	console.log(`Listening on port ${PORT}`);
-	console.log(`http://localhost:${PORT}`);
+	console.log(`Listening on http://localhost:${PORT}`);
 });
 
 // Static files
-app.use(express.static("Resources"));
+app.use(express.static(publicPath));
 
 // Socket setup
 const io = socket(server);
-
 const activeUsers = new Set();
 
-io.on("connection", function (socket) {
+io.on("connection", function(socket) {
 	console.log("Made socket connection");
-
 	socket.on("new user", function (data) {
 		socket.userId = data;
 		activeUsers.add(data);
@@ -58,7 +29,7 @@ io.on("connection", function (socket) {
 	socket.on("place piece", function (data){
 		//modify this
 		if (true){
-			io.emit("place piece", data)
+			io.emit("place piece", data);
 		}
 	})
 
