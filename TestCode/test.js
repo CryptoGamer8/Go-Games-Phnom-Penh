821dfe4ff6e5d6fd8113a86a37cbab0657efca4c
hubertzk <hubertzk@163.com>
2020-10-14 18:36:57 +0800

    changed README.md and test.js

--- TestCode/test.js
+++ TestCode/test.js
@@ -1,18 +1,1 @@
-var MongoClient = require('mongodb').MongoClient;
-
-// Connect to the db
-MongoClient.connect("mongodb://localhost:27017/gogamesphnompenh", function (err, db) {
-    
-    db.collection('Persons', function (err, collection) {
-        
-        collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
-        collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
-        collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
-        
-        db.collection('Persons').count(function (err, count) {
-            if (err) throw err;
-            
-            console.log('Total Rows: ' + count);
-        });
-    });       
-});
+console.log(__dirname);
